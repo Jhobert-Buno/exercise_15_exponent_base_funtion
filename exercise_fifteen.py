233c3fdbca45a623b06d3a96ce38580e5c67c2b9
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-23 21:51:52 +0800

    writing the given and instructions

---
+++ exercise_fifteen.py
@@ -0,0 +1,13 @@
+# Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
+
+# given
+base1 = 2
+exponent1 = 5
+
+base2 = 5
+exponent2 = 4
+
+# funtions
+
+
